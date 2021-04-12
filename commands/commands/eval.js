3563cd03529554f5da3e415e06e9b63959e59bff
frenchbones <frenchbones@outlook.com>
2021-04-13 01:10:30 +0200

    refactor: console.log() eval results

--- commands/commands/eval.js
+++ commands/commands/eval.js
@@ -5,6 +5,14 @@ const embed = require('@modules/embed.js')
 const loadYAML = require('@modules/yaml.js')
 const config = loadYAML('config')
 
+// Chalk
+const chalk = require('chalk');
+const infoPrefixColor = chalk.black.bgWhite
+const warnPrefixColor = chalk.black.bgYellow
+const errorPrefixColor = chalk.white.bgRed
+const urlColor = chalk.blue.underline
+const highlightColor = chalk.yellow
+
 module.exports = {
     commands: ['eval'],
 	description: "Evaluate javascript code",
@@ -13,7 +21,8 @@ module.exports = {
             if (config.Eval.Enabled) {
                 const args = arguments.slice(0).join(" ")
                 const evalResult = eval(args)
-                message.channel.send(embed('default', `Eval Results`, `${evalResult}`))
+                message.channel.send(embed('default', `Eval Results`, `Check console for more information\n**Result:**\n\`\`\`${evalResult}\`\`\``))
+                console.log(warnPrefixColor('Eval Result >'), evalResult)
             } else {
                 message.channel.send(embed('error', `Command Disabled`, `The eval command is disabled and should only be enabled if you know what you are doing and realise the risks`))
             }
